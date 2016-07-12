055ff8b893e1f7c4647dcd0815d2d82e0e31aefe
BlackCoyote <blackcoyoteproductions@gmail.com>
2016-07-12 07:11:44 +0200

    add this word for connor

--- src/main/java/space/morphanone/webizen/events/GetRequestScriptEvent.java
+++ src/main/java/space/morphanone/webizen/events/GetRequestScriptEvent.java
@@ -14,7 +14,7 @@ public class GetRequestScriptEvent extends BasicRequestScriptEvent {
     // <context.address> Returns the IP address of the device that sent the request.
     // <context.request> Returns the path that was requested
     // <context.query> Returns a dList of the query included with the request
-    // <context.user_info> Returns info about the user sending the request, if any.
+    // <context.user_info> Returns info about the authenticated user sending the request, if any.
     //
     // @Determine
     // Element to set the content of the response directly
